afffb2f49aaea028b8ebbdf18d88c8c7cbf953e2
Janice Cotcher <42756980+janice-cotcher@users.noreply.github.com>
2023-01-30 02:07:40 +0000

    added answers

---
+++ 07_Practice1.py
@@ -0,0 +1,11 @@
+# answers to practice 1
+
+def perimeter(length, width):
+    """
+    Calculate the perimeter of a rectangle.
+    """
+    P = 2*length + 2*width
+    print("The perimeter of the rectangle is " + str(P) + "cm")
+
+
+perimeter(16, 10)
